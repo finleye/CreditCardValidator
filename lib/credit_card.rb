cf0e2b7249b9246966e7ee35ffc275513ecc09c3
Corey Finley <finley.corey@gmail.com>
2013-11-03 14:49:56 -0500

    greatly improved the type matching by using regular expressions

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -32,71 +32,43 @@ class CreditCard
 	# | Visa       | 4           | 13 or 16      |
 	# +------------+-------------+---------------+
 
+	# test strings by regular expression
 	def type
-		# check lengths first
-		# then first digits to find type
-		if self.number.length == 16
-			if self.number[0..3] == "6011"
-				return "Discover"
-			elsif self.number[0..1] == "51" || self.number[0..1] == "55"
-				return "MasterCard"
-			elsif self.number[0] == "4"
-				return "VISA"
-			else
-				return "Unknown"
-			end
-		elsif self.number.length == 15
-			if self.number[0..1] == "34" || self.number[0..1] == "37"
-				return "AMEX"
-			else
-				return "Unknown"
-			end
-		elsif self.number.length == 13
-			if self.number[0] == "4"
-				return "VISA"
-			else
-				return "Unknown"
-			end
+		if self.number =~ /\A4(([0-9]{15})|([0-9]{12}))$/
+			return "VISA"
+		elsif self.number =~ /\A(37|34)[0-9]{13}$/
+			return "AMEX"
+		elsif self.number =~ /\A6011[0-9]{12}$/
+			return "Discover"
+		elsif self.number =~ /\A5[1-5][0-9]{14}$/
+			return "MasterCard"
 		else
 			return "Unknown"
 		end
 	end
 
-	# test the validity of a credit card number
-	# reverse the string number, and then process every other number
+	# Test the validity of a credit card number.
+	# Reverse the string number, and then process every other number
 	# by doubling it. Add each number to the sum, if the number is 
 	# greater than 9, split it into it's to parts (i.e. 17 -> 1+7)
 	# return the boolean evaluation of the sum mod ten equaling zero
 
 	def valid?
-		# According to Wikipedia, the smallest valid bank number is 13 digits
-		# so if the length is shorter than 13, then don't bother processing it
 		return false if self.number.length <= 12
 
 		sum = 0
-
-		# take the string number, reverse it, and then split it into 
-		# a collection of string digits for processing
 		self.number.reverse.split("").each_with_index do |digit, i|
-
-			# convert the string digit to an integer
 			digit = digit.to_i
 
-			# act on every other digit, starting from the second 
+			# only alter every other digit, starting from the second item
 			if (i+1)%2==0
-				digit = digit*2 
-				
-				# if the digit is greater than 9 split it into to integers, 
-				# otherwise just add it to the sum
+				digit = digit*2
 				digit = (digit%10) + ((digit-(digit%10))/10) if digit > 9
-				sum += digit
-			else
-				# if the number does not need to be doubled just add it to the sum
-				sum += digit
 			end
+
+			sum += digit
 		end
 
-		# return the boolean evaluation of sum mod ten equaling zero
 		return sum%10==0
 	end
 end
